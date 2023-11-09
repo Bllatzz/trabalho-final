c6af62badda10a7a14d1bc0f78e2aec232316de5
Bllatzz <bl4tz@hotmail.com>
2023-11-09 19:05:59 -0300

    tudo

---
+++ script/app.js
@@ -0,0 +1,17 @@
+/*passos*/
+const myObserver = new IntersectionObserver((entries) =>{
+    entries.forEach( (entry) =>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('aparecer')
+        }else{
+            entry.target.classList.remove('aparecer')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.passos')
+
+elements.forEach((element) => myObserver.observe(element))
+
+
+
